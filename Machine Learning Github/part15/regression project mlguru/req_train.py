85cebfb1d08c41e1738673bc12f5926135b1ae62
Arjun Pukale <49120008+ArjunPukale@users.noreply.github.com>
2021-03-07 21:13:51 +0530

    Add files via upload

---
+++ Machine Learning Github/part15/regression project mlguru/req_train.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import Ridge
